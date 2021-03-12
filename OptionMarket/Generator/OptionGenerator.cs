2f0d67fba2b177f9d51eef7b98b9d38753ada19b
hbehboudi <hbehboudi@ce.sharif.edu>
2021-03-12 13:19:09 +0330

    refactor

--- OptionMarket/Generator/OptionGenerator.cs
+++ OptionMarket/Generator/OptionGenerator.cs
@@ -1,8 +1,8 @@
 ﻿using OptionMarket.Extensions;
 using OptionMarket.Model;
-using OptionMarket.Model.Adapter;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace OptionMarket.Generator
 {
@@ -11,17 +11,68 @@ namespace OptionMarket.Generator
         public List<Option> Generate(DataTable table)
         {
             var stocks = new StockGenerator().Generate(table);
-            var optionAdapter = new OptionAdapter(stocks);
 
             var options = new List<Option>();
 
             for (var i = ConstValues.FirstRow; i < table.Rows.Count; i++)
             {
-                var option = optionAdapter.Create(table.Rows[i]);
+                var option = Create(table.Rows[i], stocks);
                 options.AddIfNotNull(option);
             }
 
             return options;
+        }
+
+        private Option Create(DataRow row, List<Stock> stocks)
+        {
+            var name = row[ConstValues.NameColumn].ToString();
+            var description = row[ConstValues.DescriptionColumn].ToString();
+
+            if (!IsOption(name, description))
+            {
+                return null;
+            }
+
+            var firstSeparator = description.IndexOf(ConstValues.Separator);
+            var secondSeparator = description.LastIndexOf(ConstValues.Separator);
+            var length = secondSeparator - firstSeparator - 1;
+
+            var baseName = description.Substring(0, description.IndexOf(ConstValues.Separator));
+
+            return new Option
+            {
+                Name = name,
+                Description = description,
+                Stock = stocks.Single(x => baseName[(baseName.IndexOf(" ") + 1)..].Equals(x.Name)),
+                Volume = row[ConstValues.VolumeColumn].ToLong(),
+                DueDate = description[(secondSeparator + 1)..],
+                Price = row[ConstValues.PriceColumn].ToLong(),
+                FinalPrice = row[ConstValues.FinalPriceColumn].ToLong(),
+                PriceApply = description.Substring(firstSeparator + 1, length).ToLong(),
+                BuyVolume = row[ConstValues.BuyVolumeColumn].ToLong(),
+                BuyPrice = row[ConstValues.BuyPriceColumn].ToLong(),
+                SellPrice = row[ConstValues.SellPriceColumn].ToLong(),
+                SellVolume = row[ConstValues.SellVolumeColumn].ToLong(),
+            };
+        }
+
+        private static bool IsOption(string name, string description)
+        {
+            if (!name.StartsWith(ConstValues.BuyFirstLetter) &&
+                !name.StartsWith(ConstValues.SellFirstLetter))
+            {
+                return false;
+            }
+
+            if (!description.Contains(ConstValues.Separator))
+            {
+                return false;
+            }
+
+            var firstSeparator = description.IndexOf(ConstValues.Separator);
+            var secondSeparator = description.LastIndexOf(ConstValues.Separator);
+
+            return firstSeparator != secondSeparator;
         }
     }
 }
--- OptionMarket/Generator/StockGenerator.cs
+++ OptionMarket/Generator/StockGenerator.cs
@@ -1,5 +1,5 @@
-﻿using OptionMarket.Model;
-using OptionMarket.Model.Adapter;
+﻿using OptionMarket.Extensions;
+using OptionMarket.Model;
 using System.Collections.Generic;
 using System.Data;
 
@@ -10,15 +10,28 @@ namespace OptionMarket.Generator
         public List<Stock> Generate(DataTable table)
         {
             var stocks = new List<Stock>();
-            var stockAdapter = new StockAdapter();
             
             for (var i = ConstValues.FirstRow; i < table.Rows.Count; i++)
             {
-                var stock = stockAdapter.Create(table.Rows[i]);
+                var stock = Create(table.Rows[i]);
                 stocks.Add(stock);
             }
 
             return stocks;
         }
+
+        private Stock Create(DataRow row) =>
+            new Stock
+            {
+                Name = row[ConstValues.NameColumn].ToString(),
+                Description = row[ConstValues.DescriptionColumn].ToString(),
+                Volume = row[ConstValues.VolumeColumn].ToLong(),
+                Price = row[ConstValues.PriceColumn].ToLong(),
+                FinalPrice = row[ConstValues.FinalPriceColumn].ToLong(),
+                BuyVolume = row[ConstValues.BuyVolumeColumn].ToLong(),
+                BuyPrice = row[ConstValues.BuyPriceColumn].ToLong(),
+                SellPrice = row[ConstValues.SellPriceColumn].ToLong(),
+                SellVolume = row[ConstValues.SellVolumeColumn].ToLong(),
+            };
     }
 }
